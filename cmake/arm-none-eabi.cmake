a969c6f3ddc285f9d7e7bdc9eae1e87c246a5bfc
91tleg <maxchou0417@gmail.com>
2026-08-30 15:36:49 -0700

    build: clean up cmake target structure
    - Remove architecture-specific flags from targets
    - Apply Cortex-M7 flags through the ARM toolchain
    - Remove conditional hardware/test cmake configuration

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -5,3 +5,13 @@ set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(MCU_FLAGS
+    -mcpu=cortex-m7
+    -mthumb
+    -mfpu=fpv5-d16
+    -mfloat-abi=hard
+)
+
+add_compile_options(${MCU_FLAGS})
+add_link_options(${MCU_FLAGS})
